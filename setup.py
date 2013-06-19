2604ada80be030e4d63725e81296d6b04befbfa6
Adam Charnock <adam@omniwiki.co.uk>
2013-06-19 22:35:54 +0100

    Finalising packaging

--- setup.py
+++ setup.py
@@ -11,19 +11,22 @@ from tz_detect import __version__
 setup(
     name='django-tz-detect',
     version=__version__,
-    # Your name & email here
-    author='',
-    author_email='',
-    # If you had django_tz_detect.tests, you would also include that in this list
+    author='Adam Charnock',
+    author_email='adam@adamcharnock.com',
     packages=['tz_detect'],
+    package_data={
+        'tz_detect': [
+            'templates/tz_detect/detector.html',
+            'static/tz_detect/js/tzdetect.js',
+            'static/tz_detect/js/tzdetect.min.js',
+        ]
+    },
     # Any executable scripts, typically in 'bin'. E.g 'bin/do-something.py'
     scripts=[],
     # REQUIRED: Your project's URL
-    url='',
-    # Put your license here. See LICENSE.txt for more information
-    license='',
-    # Put a nice one-liner description here
-    description='',
+    url='https://github.com/adamcharnock/django-tz-detect',
+    license='MIT',
+    description='Automatic user timezone detection for django',
     long_description=open('README.rst').read() if exists("README.rst") else "",
     # Any requirements here, e.g. "Django >= 1.1.1"
     install_requires=[
