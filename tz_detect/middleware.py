80faf5b391beb9cc5507b89026f0bfd2350f68c2
Doug Cox <dougcoxs1@gmail.com>
2014-07-03 16:55:09 -0700

    test if BaseTzInfo object is stored in existing sessions in middleware

--- tz_detect/middleware.py
+++ tz_detect/middleware.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from pytz.tzinfo import BaseTzInfo
 from tz_detect.utilities import offset_to_timezone
 
 
@@ -9,6 +10,10 @@ class TimezoneMiddleware(object):
             # `request.timezone_active` is used in the template
             # tag to detect if the timezone has been activated
             request.timezone_active = True
-            timezone.activate(offset_to_timezone(tz))
+            # for existing sessions storing BaseTzInfo objects
+            if type(tz) == BaseTzInfo:
+                timezone.activate(tz)
+            else:
+                timezone.activate(offset_to_timezone(tz))
         else:
             timezone.deactivate()
