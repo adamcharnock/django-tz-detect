daeaab34c14cd82c653eab4ae7be5e4b119bb46d
bashu <basil.shubin@gmail.com>
2015-05-23 12:21:26 +0600

    use ``static`` template tag instead of STATIC_URL

--- tz_detect/templatetags/tz_detect.py
+++ tz_detect/templatetags/tz_detect.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django import template
 from django.conf import settings
 
@@ -8,6 +10,5 @@ register = template.Library()
 def tz_detect(context):
     return {
         'show': not hasattr(context['request'], 'timezone_active'),
-        'STATIC_URL': settings.STATIC_URL,
-        'DEBUG': settings.DEBUG,
+        'debug': getattr(settings, 'DEBUG', False),
     }
