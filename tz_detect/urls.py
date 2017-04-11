a68bff67db4de022fa81fb36221d7d4c2e07d58d
Drew Hubl <drew.hubl@gmail.com>
2017-04-11 06:44:12 -0600

    Make /set exempt from Django's CSRF middleware

--- tz_detect/urls.py
+++ tz_detect/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import SetOffsetView
 
 urlpatterns = [
-    url(r'^set/$', SetOffsetView.as_view(), name="tz_detect__set"),
+    url(r'^set/$', csrf_exempt(SetOffsetView.as_view()), name="tz_detect__set"),
 ]
