15cbd28775be86f459b804f93ed549e87a6ad2ad
Craig Anderson <craiga@craiga.id.au>
2019-11-24 20:52:15 +0000

    Set attributes on script tag.

--- tz_detect/templatetags/tz_detect.py
+++ tz_detect/templatetags/tz_detect.py
@@ -8,11 +8,12 @@ register = template.Library()
 
 
 @register.inclusion_tag('tz_detect/detector.html', takes_context=True)
-def tz_detect(context):
+def tz_detect(context, **script_attrs):
     return {
         'show': not hasattr(context.get('request'), 'timezone_active'),
         'debug': getattr(settings, 'DEBUG', False),
         'csrf_header_name': convert_header_name(
             getattr(settings, 'CSRF_HEADER_NAME', 'HTTP_X_CSRFTOKEN')
         ),
+        'script_attrs': script_attrs,
     }
